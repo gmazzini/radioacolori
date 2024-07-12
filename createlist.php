cdd3e0853732eee7947ba679923c203cbbe5a8c4
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-12 07:04:13 +0200

    Update createlist.php

--- createlist.php
+++ createlist.php
@@ -10,7 +10,10 @@ for(;;){
   $id=$row["id"];
   $tt=$row["tt"];
   $ttt+=$tt;
-  echo "$id\n";
+  echo $p1."intro.ogg\n";
+  for($i=0;$i<5;$i++)echo $p1."n".substr($id,$i,1).".ogg\n";
+  echo $p1."coda.ogg\n";
+  echo $p2."$id.ogg\n";
 }
 mysqli_free_result($query);
 mysqli_close($con);
