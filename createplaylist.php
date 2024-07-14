3f64ec13eeeb920540f74a8ca6d7feef538d5acc
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-14 12:59:25 +0200

    Update createplaylist.php

--- createplaylist.php
+++ createplaylist.php
@@ -1,13 +1,13 @@
 <?php
 include "local.php";
-$playlist=(int)$argv[1];
+$tt=time()/86400+1;
 $special=array("RADIOAMATORI","SCIENZA","STORIE DEL NAVILE");
 $avoid=array("INNOVAZIONE");
 $runm=7;
 $con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 $p1="/home/ices/music/voice/";
 $p2="/home/ices/music/ogg04/";
-$fp=fopen("/home/ices/playlist_$playlist.txt","wt");
+$fp=fopen("/home/ices/playlist_$tt.txt","wt");
 
 $listout="("; $listin="(";
 $co=0;
@@ -23,35 +23,35 @@ foreach($avoid as $k => $v){
 }
 $listout.=")"; $listin.=")";
 
-$query=mysqli_query($con,"select id,tt from track where score=2 and genre not in $listout order by rand()");
+$query=mysqli_query($con,"select id,duration from track where score=2 and genre not in $listout order by rand()");
 $nm2=0;
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $idm2[$nm2]=$row["id"];
-  $ttm2[$nm2]=$row["tt"];
+  $durationm2[$nm2]=$row["duration"];
   $nm2++;
 }
 mysqli_free_result($query);
 
 $nm1=0;
-$query=mysqli_query($con,"select id,tt from track where score=1 and genre not in $listout order by tt");
+$query=mysqli_query($con,"select id,duration from track where score=1 and genre not in $listout order by tt");
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $idm1[$nm1]=$row["id"];
-  $ttm1[$nm1]=$row["tt"];
+  $durationm1[$nm1]=$row["duration"];
   $nm1++;
 }
 mysqli_free_result($query);
 
-$query=mysqli_query($con,"select id,tt from track where score=2 and genre in $listin order by rand()");
+$query=mysqli_query($con,"select id,duration from track where score=2 and genre in $listin order by rand()");
 $nc=0;
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $idc[$nc]=$row["id"];
-  $ttc[$nc]=$row["tt"];
+  $durationc[$nc]=$row["duration"];
   $nc++;
 }
 mysqli_free_result($query);
@@ -61,36 +61,37 @@ $nq1=(int)($nm1/$nc);
 $nq2=(int)($nm2/$nc);
 $iq1=$iq2=0;
 $um1=$um2=$uc=0;
-$ttt=0;
+$duration=0;
 $el=0;
 for($i=0;$i<$nc;$i++){
   for($q=0;$q<=$runm;$q++){
     if($q==$runm){
       $ida=$idc[$i];
-      $tta=$ttc[$i];
+      $durationa=$durationc[$i];
       $uc++;
     }
     else if($q<$nq2){
       $ida=$idm2[$iq2];
-      $tta=$ttm2[$iq2];
+      $durationa=$$durationa2[$iq2];
       $iq2++;
       if($iq2>=$nm2)$iq2=0;
       $um2++;
     }
     else {
       $ida=$idm1[$iq1];
-      $tta=$ttm1[$iq1];
+      $durationa=$durationm1[$iq1];
       $iq1++;
       if($iq1>=$nm1)$iq1=0;
       $um1++;
     }
-    mysqli_query($con,"insert into playlist (playlist,id,position) values ($playlist,'$ida',$el)");
+    mysqli_query($con,"insert into playlist (tt,id,position) values ($tt,'$ida',$el)");
     $el++;
     fprintf($fp,"%s%s.ogg\n",$p2,$ida);
     fprintf($fp,"%sintro.ogg\n",$p1);
     for($j=0;$j<5;$j++)fprintf($fp,"%sn%s.ogg\n",$p1,substr($ida,$j,1));
     fprintf($fp,"%scoda.ogg\n",$p1);
-    $ttt+=$tta;
+    $duration+=$durationa;
+    if($duration>86400)break;
   }
 }
 mysqli_close($con);
