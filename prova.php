f81cc2ce9f444aa8249637809a5e9e10920226ea
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-15 09:13:14 +0200

    Update prova.php

--- prova.php
+++ prova.php
@@ -7,6 +7,7 @@ $avoid=array("INNOVAZIONE");
 $con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 $p1="/home/ices/music/voice/";
 $p2="/home/ices/music/ogg04/";
+$fp=fopen("/home/ices/playlistNEW.txt","wt");
 
 $listout="("; $listin="(";
 $co=0;
@@ -85,48 +86,48 @@ for(;;){
 }
 mysqli_free_result($query);
 
-
-exit(1);
-
+mysqli_query($con,"delete from playlist where tt=$tt");
 $nq1=(int)($nm1/$nc);
 $nq2=(int)($nm2/$nc);
 $iq1=$iq2=0;
 $um1=$um2=$uc=0;
 $duration=0;
 $el=0;
+
 for($i=0;$i<$nc;$i++){
   for($q=0;$q<=$runm;$q++){
     if($q==$runm){
       $ida=$idc[$i];
-      $durationa=$durationc[$i];
       $uc++;
     }
     else if($q<$nq2){
       $ida=$idm2[$iq2];
-      $durationa=$durationm2[$iq2];
-      $iq2++;
-      if($iq2>=$nm2)$iq2=0;
+      if(++$iq2>=$nm2)$iq2=0;
       $um2++;
     }
     else {
       $ida=$idm1[$iq1];
-      $durationa=$durationm1[$iq1];
-      $iq1++;
-      if($iq1>=$nm1)$iq1=0;
+      if(++$iq1>=$nm1)$iq1=0;
       $um1++;
     }
-
-
-
     
+    mysqli_query($con,"insert into playlist (tt,id,position) values ($tt,'$ida',$el)");
+    mysqli_query($con,"update track set used=used+1 where id='$ida'");
     $el++;
-   
-    $duration+=$durationa;
+    fprintf($fp,"%s%s.ogg\n",$p2,$ida);
+    fprintf($fp,"%sintro.ogg\n",$p1);
+    for($j=0;$j<5;$j++)fprintf($fp,"%sn%s.ogg\n",$p1,substr($ida,$j,1));
+    fprintf($fp,"%scoda.ogg\n",$p1);
+    
+    $query=mysqli_query($con,"select duration from track where id='$ida'");
+    $row=mysqli_fetch_assoc($query);
+    $duration+=$row["duration"];
+    mysqli_free_result($query);
     if($duration>86400)break;
   }
 }
 mysqli_close($con);
-
+fclose($fp);
 echo "$duration $nm2:$um2 $nm1:$um1 $nc:$uc\n";
 
 ?>
