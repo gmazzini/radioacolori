c8432140804ffb0019e7a3347641a8156fb26680
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-15 21:09:03 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -36,14 +36,22 @@ for($ts=0;;$ts++){
 mysqli_free_result($query);
 $f=$pp-2; if($f<0)$f=0;
 $t=$pp+2; if($t>=$ts)$t=$ts-1;
+$vv=$xx;
+for($i=$f;$i<$pp;$i++){
+  $query=mysqli_query($con,"select duration from track where id='$seq[$i]'");
+  $row=mysqli_fetch_assoc($query);
+  $vv-=(int)$row["duration"];
+  mysqli_free_result($query);
+}
 for($i=$f;$i<=$t;$i++){
   $query=mysqli_query($con,"select title,author,genre,duration from track where id='$seq[$i]'");
   $row=mysqli_fetch_assoc($query);
-  echo $seq[$i];
+  echo date("H:i",$vv)." | ".$seq[$i];
   echo " | ".$row["title"];
   echo " | ".$row["author"];
   echo " | ".$row["genre"];
   echo " | ".(int)$row["duration"]."s\n";
+  $vv+=(int)$row["duration"];
   mysqli_free_result($query);
 }
 
