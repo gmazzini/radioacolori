ff47e63ac190250be8bd08580fb7058efb421eb2
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-15 19:36:13 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,15 +1,25 @@
 <?php
+include "local.php";
 date_default_timezone_set("Europe/Rome");
+$con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+$tt=(int)(time()/86400);
+
 echo "<pre>";
 $ll=file("/var/log/ices/ices.log");
 for($i=count($ll)-1;$i>0;$i--)
   if(strpos($ll[$i],"Currently playing")!==false)
     break;
 $id=current(explode(".",end(explode("/",$ll[$i]))));
-$tt=strtotime(substr($ll[$i],1,20));
+$xx=strtotime(substr($ll[$i],1,20));
 
-echo "$id $tt\n";
-echo time()-$tt."\n";
-  
+echo "$id $xx\n";
+echo time()-$xx."\n";
+
+$query=mysqli_query($con,"select title,author,duration from track id='$id'");
+$row=mysqli_fetch_assoc($query);
+echo $row["title"]." ".$row["author"]." ".$row["duration"]."\n";
+mysqli_free_result($query);
+
+
   
 ?>
