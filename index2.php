4c81f765ae2e6d58d60edd761bd430aaa916ee76
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-15 19:08:19 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -4,7 +4,11 @@ $ll=file("/var/log/ices/ices.log");
 for($i=count($ll)-1;$i>0;$i--)
   if(strpos($ll[$i],"Currently playing")!==false)
     break;
+
+$aux=end(explode("/",$ll[$i]));
+echo "$aux\n";
 echo $ll[$i]."\n";
+
   
   
 ?>
