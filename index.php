5c56fc2f826350c3a5840996060215dced5fc9a7
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-15 23:03:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ echo "</script>\n";
 
 echo "<pre><table>";
 echo "<td><img src='logo.jpg' width='10%' height='auto'></td>";
-echo "<td><form method='post'><input type='text' id='myid'><input type='submit' value='Cerca'></td>";
+echo "<td><form method='post'><input type='text' id='myid'><input type='submit' value='Cerca'></form></td>";
 $ids=$_POST["myid"];
 echo $ids."...\n";
 $query1=mysqli_query($con,"select title,author,genre,duration from track where id='$ids'");
