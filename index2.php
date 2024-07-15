0b67058551c2ca0e1e70042c3607df74414348a3
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-15 20:50:43 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -27,19 +27,18 @@ mysqli_free_result($query);
 
 echo "<font color='blue'>Palinsesto\n</font>";
 $query=mysqli_query($con,"select id from playlist where tt=$tt order by position");
-for($i=0;;$i++){
+for($ts=0;;$ts++){
   $row=mysqli_fetch_assoc($query);
+  if($row==null)break;
+  $seq[$ts]=$row["id"];
+  if($seq[$ts]==$id)$pp=$ts;
+}
 mysqli_free_result($query);
-
-  
-echo "Titolo: ".$row["title"]."\n";
-echo "Autore: ".$row["author"]."\n";
-echo "Genere: ".$row["genre"]."\n";
-echo "Durata: ".(int)$row["duration"]."s\n";
-echo "Inizio: ".date("Y-m-d H:i:s",$xx)."\n";
-echo "Identificativo: ".$id."\n\n";
-mysqli_free_result($query);
-
+$f=$pp-2; if($f<0)$f=0;
+$t=$pp+2; if($t>=$ts)$t=$ts-1;
+for($i=$f;$i<=$t;$i++){
+  echo $seq[$i]."\n";
+}
 
 echo "Prossimo brano tra: ".$xx+(int)$row["duration"]-time()."s\n";
 
