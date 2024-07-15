f3c9cb3211d561809ecd97295dba114f6fc4218c
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-15 14:18:14 +0200

    Update createplaylist.php

--- createplaylist.php
+++ createplaylist.php
@@ -23,35 +23,65 @@ foreach($avoid as $k => $v){
 }
 $listout.=")"; $listin.=")";
 
-$query=mysqli_query($con,"select id,duration from track where score=2 and genre not in $listout order by rand()");
+function myshuffle(&$a,$f,$t){
+  for($j=$t;$j>$f;$j--){
+    $r=rand($f,$t);
+    $aux=$a[$j];
+    $a[$j]=$a[$r];
+    $a[$r]=$aux;
+  }
+}
+
+$query=mysqli_query($con,"select id,used from track where score=2 and genre not in $listout order by used");
 $nm2=0;
+$fromsh=0;
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $idm2[$nm2]=$row["id"];
-  $durationm2[$nm2]=$row["duration"];
+  $auxused=$row["used"];
+  if($nm2==0)$lastused=$auxused;
+  elseif($lastused<>$auxused){
+    myshuffle($idm2,$fromsh,$nm2-1);
+    $fromsh=$nm2;
+    $lastused=$auxused;
+  }
   $nm2++;
 }
 mysqli_free_result($query);
 
+$query=mysqli_query($con,"select id,used from track where score=1 and genre not in $listout order by used");
 $nm1=0;
-$query=mysqli_query($con,"select id,duration from track where score=1 and genre not in $listout order by rand()");
+$fromsh=0;
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $idm1[$nm1]=$row["id"];
-  $durationm1[$nm1]=$row["duration"];
+  $auxused=$row["used"];
+  if($nm1==0)$lastused=$auxused;
+  elseif($lastused<>$auxused){
+    myshuffle($idm1,$fromsh,$nm1-1);
+    $fromsh=$nm1;
+    $lastused=$auxused;
+  }
   $nm1++;
 }
 mysqli_free_result($query);
 
-$query=mysqli_query($con,"select id,duration from track where score=2 and genre in $listin order by rand()");
+$query=mysqli_query($con,"select id,used from track where score=2 and genre in $listin order by used");
 $nc=0;
+$fromsh=0;
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
   $idc[$nc]=$row["id"];
-  $durationc[$nc]=$row["duration"];
+  $auxused=$row["used"];
+  if($nc==0)$lastused=$auxused;
+  elseif($lastused<>$auxused){
+    myshuffle($idc,$fromsh,$nc-1);
+    $fromsh=$nc;
+    $lastused=$auxused;
+  }
   $nc++;
 }
 mysqli_free_result($query);
@@ -63,27 +93,24 @@ $iq1=$iq2=0;
 $um1=$um2=$uc=0;
 $duration=0;
 $el=0;
+
 for($i=0;$i<$nc;$i++){
   for($q=0;$q<=$runm;$q++){
     if($q==$runm){
       $ida=$idc[$i];
-      $durationa=$durationc[$i];
       $uc++;
     }
     else if($q<$nq2){
       $ida=$idm2[$iq2];
-      $durationa=$durationm2[$iq2];
-      $iq2++;
-      if($iq2>=$nm2)$iq2=0;
+      if(++$iq2>=$nm2)$iq2=0;
       $um2++;
     }
     else {
       $ida=$idm1[$iq1];
-      $durationa=$durationm1[$iq1];
-      $iq1++;
-      if($iq1>=$nm1)$iq1=0;
+      if(++$iq1>=$nm1)$iq1=0;
       $um1++;
     }
+    
     mysqli_query($con,"insert into playlist (tt,id,position) values ($tt,'$ida',$el)");
     mysqli_query($con,"update track set used=used+1 where id='$ida'");
     $el++;
@@ -91,7 +118,11 @@ for($i=0;$i<$nc;$i++){
     fprintf($fp,"%sintro.ogg\n",$p1);
     for($j=0;$j<5;$j++)fprintf($fp,"%sn%s.ogg\n",$p1,substr($ida,$j,1));
     fprintf($fp,"%scoda.ogg\n",$p1);
-    $duration+=$durationa;
+    
+    $query=mysqli_query($con,"select duration from track where id='$ida'");
+    $row=mysqli_fetch_assoc($query);
+    $duration+=$row["duration"];
+    mysqli_free_result($query);
     if($duration>86400)break;
   }
 }
