af42166258019de84aec831e34f10c2b152b23f9
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-15 19:15:34 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -6,7 +6,7 @@ for($i=count($ll)-1;$i>0;$i--)
   if(strpos($ll[$i],"Currently playing")!==false)
     break;
 $id=current(explode(".",end(explode("/",$ll[$i]))));
-$tt=strtotime(substr($ll[$i],1,20))
+$tt=strtotime(substr($ll[$i],1,20));
 
 echo "$id $tt\n";
 echo $ll[$i]."\n";
