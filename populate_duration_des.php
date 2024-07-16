70ab59e0ff16c252a1a13313f48e0723ada68af6
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-16 15:35:30 +0200

    Update populate_duration_des.php

--- populate_duration_des.php
+++ populate_duration_des.php
@@ -7,7 +7,9 @@ for(;;){
   if($row==null)break;
   $id=$row["id"];
   $oo=json_decode(shell_exec("ffprobe -i music/ogg04v/$id.ogg -v quiet -print_format json -show_streams -hide_banner"),true);
-  print_r($oo); exit(1);
+  $duration_des=$oo["streams"][0]["duration"];
+  print_r($oo); 
+  echo $duration_des; exit(1);
 }
 mysqli_free_result($query);
 mysqli_close($con);
