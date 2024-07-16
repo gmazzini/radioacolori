665c9d3a2e144099d1f587f513f1d93b49aa7091
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-16 15:32:01 +0200

    Create populate_duration_des.php

---
+++ populate_duration_des.php
@@ -0,0 +1,16 @@
+<?php
+include "local.php";
+$con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
+$query=mysqli_query($con,"select id from track where duration_des=0.0");
+for(;;){
+  $row=mysqli_fetch_assoc($query);
+  if($row==null)break;
+  $id=$row["id"];
+  $oo=json_decode(shell_exec("ffprobe -i ogg04v/$id.ogg -v quiet -print_format json -show_streams -hide_banner"),true);
+  print_r($oo); ecit(1);
+}
+mysqli_free_result($query);
+
+
+
+?>
