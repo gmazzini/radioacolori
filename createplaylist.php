3f31cea95e8c71750fc628a7631526226ebd5a81
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-20 14:07:29 +0200

    Update createplaylist.php

--- createplaylist.php
+++ createplaylist.php
@@ -93,6 +93,7 @@ mysqli_free_result($query);
 
 mysqli_query($con,"delete from playlist where tt=$tt");
 $hitm2=$maxduration2/86400*$ratio/($ratio+1);
+echo "ratio=$ratio hitm2=$hitm2 maxsuration2=$maxduration2\n";
 if($hitm2>100)$hitm2=100;
 $ic=$iq1=$iq2=0;
 $um1=$um2=$uc=0;
