63c86a6ae43e317bb95c9696f813b43464540547
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-21 16:43:48 +0200

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -33,14 +33,14 @@ for($j=0;$j<$i;$j++){
 mysqli_close($con);
 
 function mystr($a,$l){
-  $la=strlen($a);
+  $la=mb_strlen($a);
   if($la>=$l)return substr($a,0,$l-3)."...";
   if($la==$l-1)return substr($a,0,$l-2)."..";
   if($la==$l-2)return substr($a,0,$l-2).".";
   return $a.str_repeat(" ",$l-$la);
 }
 function mystr2($a,$l){
-  $la=strlen($a);
+  $la=mb_strlen($a);
   return $a.str_repeat(" ",$l-$la);
 }
 ?>
