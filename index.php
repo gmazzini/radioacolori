a37f48813a0e22aa86bed48b363a853b66a0831f
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-21 16:55:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -87,9 +87,7 @@ mysqli_close($con);
 
 function mystr($a,$l){
   $la=mb_strlen($a);
-  if($la>=$l)return substr($a,0,$l-3)."...";
-  if($la==$l-1)return substr($a,0,$l-2)."..";
-  if($la==$l-2)return substr($a,0,$l-2).".";
+  if($la>=$l)return mb_substr($a,0,$l-1).">";
   return $a.str_repeat(" ",$l-$la);
 }
 
