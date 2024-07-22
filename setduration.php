4992b14504d9ba8020d412a70e8a6134f8ac6ce6
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-22 20:35:10 +0200

    Update setduration.php

--- setduration.php
+++ setduration.php
@@ -14,42 +14,8 @@ for(;;){
   $dd=json_decode(shell_exec("ffprobe -v quiet -print_format json -show_streams -hide_banner $p3/$id.ogg"),true);
   $duration_extra=$dd["streams"][0]["duration"];
   echo "$id $duration $duration_extra\n";
-  
-  exit(0);
-  
-
-
+  mysqli_query($con,"update track set duration=$duration,duration_extra=$duration_extra where id='id'");
 }
 mysqli_free_result($query);
 
-echo "<pre>";
-$vv=86400-60;;
-for($j=0;$j<$i;$j++){
-  $query=mysqli_query($con,"select title,author,genre,duration,used,score from track where id='$id[$j]'");
-  $row=mysqli_fetch_assoc($query);
-  $zz=in_array($row["genre"],$special);
-  if($zz)echo "<font color='blue'>";
-  echo date("H:i:s",$vv)." | ".$id[$j];
-  echo " | ".mystr($row["title"],40);
-  echo " | ".mystr($row["author"],30);
-  echo " | ".mystr($row["genre"],20);
-  echo " | ".mystr2((int)$row["duration"],4)."s";
-  echo " | ".mystr2($row["used"],3);
-  echo " | ".mystr2($row["score"],1);
-  if($zz)echo "</font>";
-  echo "\n";
-  $vv=$vv+$row["duration"]+$dtq;
-  mysqli_free_result($query);
-}
-mysqli_close($con);
-
-function mystr($a,$l){
-  $la=mb_strlen($a);
-  if($la>=$l)return mb_substr($a,0,$l-1).">";
-  return $a.str_repeat(" ",$l-$la);
-}
-function mystr2($a,$l){
-  $la=mb_strlen($a);
-  return $a.str_repeat(" ",$l-$la);
-}
 ?>
