b738f9dbb05d7a769f52dc763e95cb53362f9332
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-23 06:24:51 +0200

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -28,7 +28,7 @@ for($j=0;$j<$i;$j++){
   $zz=in_array($row["genre"],$special);
   if($zz)echo "<font color='blue'>";
   echo date("H:i:s",$vv)." | ".$id[$j];
-  echo " | ".date("H:i:s",$logtime[$id[$j]]);
+  if(isset($logtime[$id[$j]]))echo " | ".date("H:i:s",$logtime[$id[$j]]);
   echo " | ".mystr($row["title"],40);
   echo " | ".mystr($row["author"],30);
   echo " | ".mystr($row["genre"],20);
