249bfc9a01e0db05faa3ba1f175e8cbabf914ad2
Gianluca Mazzini <gianluca@mazzini.org>
2024-07-23 14:32:23 +0200

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -2,7 +2,7 @@
 include "local.php";
 $tt=(int)(time()/86400);
 $con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
-$corr=0.45;
+$corr=0.49;
 
 $query=mysqli_query($con,"select id from playlist where tt=$tt order by position");
 for($i=0;;$i++){
