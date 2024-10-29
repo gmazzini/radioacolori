2bffbeda3545c91006b7af55dd81f17b93c34c4c
Gianluca Mazzini <gianluca@mazzini.org>
2024-10-29 05:42:35 +0100

    Update getduration.php

--- getduration.php
+++ getduration.php
@@ -3,11 +3,11 @@ include "local.php";
 $con=mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname);
 
 
-$query=mysqli_query($con,"select id,duration from track where duration>0 and score>0");
+$query=mysqli_query($con,"select id,duration,used,last from track where duration>0 and score>0");
 for(;;){
   $row=mysqli_fetch_assoc($query);
   if($row==null)break;
-  echo $row["id"].",".$row["duration"]."\n";
+  echo $row["id"].",".$row["duration"].",".$row["used"].",".$row["last"]."\n";
   
 }
 mysqli_free_result($query);
