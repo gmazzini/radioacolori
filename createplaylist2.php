bf64b33122f2dadec6e47521013e8dceb42083e8
Gianluca Mazzini <gianluca@mazzini.org>
2024-10-31 14:22:39 +0100

    Update createplaylist2.php

--- createplaylist2.php
+++ createplaylist2.php
@@ -57,6 +57,7 @@ for(;;){
   printf(">>>> %s\n",$row["id"]);
   if(((int)$row["gsel"])==1){
     $gid=$row["gid"];
+    printf("select min(last),max(last) from track where gid='$gid'\n");
     $query2=mysqli_query($con,"select min(last),max(last) from track where gid='$gid'");
     $row2=mysqli_fetch_row($query);
     $lastmin=(isset($row2[0]))?$row2[0]:0;
