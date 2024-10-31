469b104a8979a382e8e4cd5c41f8eab2d5c72745
Gianluca Mazzini <gianluca@mazzini.org>
2024-10-31 14:30:27 +0100

    Update createplaylist2.php

--- createplaylist2.php
+++ createplaylist2.php
@@ -71,7 +71,6 @@ for(;;){
       $row2=mysqli_fetch_assoc($query2);
       if($row2==null)break;
       $idc[$nc++]=$row2["id"];
-      printf("-- %s\n",$row2["id"]);
       $group_element++;
       $group_time+=$row2["duration"];
       if($group_time>$limit_group_time || $group_element>$limit_group_element)break;
