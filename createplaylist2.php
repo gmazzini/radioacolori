a9a1f4b929736fcaf0d9023eab2b4db032806887
Gianluca Mazzini <gianluca@mazzini.org>
2024-10-31 14:19:12 +0100

    Update createplaylist2.php

--- createplaylist2.php
+++ createplaylist2.php
@@ -57,7 +57,7 @@ for(;;){
   printf(">>>> %s\n",$row["id"]);
   if(((int)$row["gsel"])==1){
     $gid=$row["gid"];
-    $query2=mysqli_query($con,"select min(last),max(last) from track where score=2 and gid='$gid'");
+    $query2=mysqli_query($con,"select min(last),max(last) from track where gid='$gid'");
     $row2=mysqli_fetch_row($query);
     $lastmin=(isset($row2[0]))?$row2[0]:0;
     $lastmax=(isset($row2[1]))?$row2[1]:0;
@@ -65,8 +65,8 @@ for(;;){
     else $lastlim=$lastmax+1;
     printf("@@@ %s %f %f %f\n",$gid,$row2[0],$row2[1],$lastmin);
     mysqli_free_result($query2);
-    printf("select id,duration from track where score=2 and gid='$gid' and last<$lastlim order by gsel asc\n");
-    $query2=mysqli_query($con,"select id,duration from track where score=2 and gid='$gid' and last<$lastlim order by gsel asc");
+    printf("select id,duration from track where gid='$gid' and last<$lastlim order by gsel asc\n");
+    $query2=mysqli_query($con,"select id,duration from track where gid='$gid' and last<$lastlim order by gsel asc");
     $group_time=0.0;
     $group_element=0;
     for(;;){
